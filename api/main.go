afc6b156dd9e57aa364dec304981fbc8ed8442d1
borislav nikolov <jack@sofialondonmoskva.com>
2019-03-05 22:25:07 +0100

    len

--- api/main.go
+++ api/main.go
@@ -429,6 +429,7 @@ func main() {
 			return
 		}
 		defer file.Close()
+		c.Header("Content-Length", fmt.Sprintf("%d", fo.Size))
 		c.Header("Content-Transfer-Encoding", "binary")
 		c.Header("Content-Disposition", "attachment; filename="+fo.SHA256) // make sure people dont use it for loading js
 		c.Header("Content-Type", "application/octet-stream")
