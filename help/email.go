f8205a3dc110c68bd6f9712d9648da0c3286ebd0
borislav nikolov <jack@sofialondonmoskva.com>
2019-03-06 20:25:24 +0100

    space

--- help/email.go
+++ help/email.go
@@ -6,13 +6,13 @@ We just received subscription cancellation message from paypal.
 You will be able to upload/download backups for 1 more month.
 If you want to renew your subscription go to:
 
-  https://baxx.dev/sub/{{.PaymentID}}
+https://baxx.dev/sub/{{.PaymentID}}
 
 and you will be redirected to paypal.com.
 
 You can check the account status with:
 
-  curl -u {{.Email}} -XPOST https://baxx.dev/protected/status | json_pp
+curl -u {{.Email}} -XPOST https://baxx.dev/protected/status | json_pp
 
 Thanks for using baxx.dev,
 if you have any feedback please send me an email to jack@baxx.dev.
@@ -30,12 +30,12 @@ the support.
 
 ## Plan (only one for now):
 
-  Storage 10G
-  Trial 1 Month 0.1E
-  Subscription: 5E per Month
-  Availability: ALPHA
+Storage 10G
+Trial 1 Month 0.1E
+Subscription: 5E per Month
+Availability: ALPHA
 
-  Here be Dragons! Data can be lost!
+Here be Dragons! Data can be lost!
 
 ## Subscribe
 
@@ -59,8 +59,8 @@ Thanks again!
 
 # Tokens
 
- Tokens are like backup namespaces, you can have the same file in
- different tokens and it won't conflict.
+Tokens are like backup namespaces, you can have the same file in
+different tokens and it won't conflict.
 
 ## Current Tokens:
 
@@ -72,117 +72,118 @@ Thanks again!
 {{end}}
 ## Create New Tokens:
 
- curl -u {{ .Email }} -d '{"write_only":false, "keep_n_versions":7}' \
-   https://baxx.dev/protected/create/token
+curl -u {{ .Email }} \
+ -d '{"write_only":false, "keep_n_versions":7}' \
+ https://baxx.dev/protected/create/token
 
- Write Only:
-   tokens can only add but not get files (without password)
+Write Only:
+ tokens can only add but not get files (without password)
 
- Keep #N Versions:
-   How many versions per file (with different sha256) to keep.  Useful
-   for database or modified files archives like, e.g:
+Keep #N Versions:
+ How many versions per file (with different sha256) to keep.  Useful
+ for database or modified files archives like, e.g:
 
-   mysqldump | curl --data-binary @- \
-    https://baxx.dev/io/$TOKEN/mysql.gz
+ mysqldump | curl --data-binary @- \
+  https://baxx.dev/io/$TOKEN/mysql.gz
 
 ## Delete tokens
 
- curl -u {{ .Email }} -d '{"uuid": "TOKEN-UUID"}' \
-   https://baxx.dev/protected/delete/token
+curl -u {{ .Email }} -d '{"uuid": "TOKEN-UUID"}' \
+ https://baxx.dev/protected/delete/token
 
- this will delete the token and all the files in it
+this will delete the token and all the files in it
 
 # File operations
 
 ## File Upload:
 
- cat path/to/file | curl --data-binary @- \
-   https://baxx.dev/io/$TOKEN/path/to/file
+cat path/to/file | curl --data-binary @- \
+ https://baxx.dev/io/$TOKEN/path/to/file
 
- Same filepath can have up to #N Versions depending on the token
- configuration.
+Same filepath can have up to #N Versions depending on the token
+configuration.
 
- Uploading the same sha256 resulting in reusing existing version and
- also does not consume quota.
+Uploading the same sha256 resulting in reusing existing version and
+also does not consume quota.
 
 ## File Download:
 
- curl https://baxx.dev/io/$TOKEN/path/to/file > file
+curl https://baxx.dev/io/$TOKEN/path/to/file > file
 
- Downloads the last upload version
+Downloads the last upload version
 
 ## File Delete:
 
- curl -XDELETE https://baxx.dev/io/$TOKEN/path/to/file
+curl -XDELETE https://baxx.dev/io/$TOKEN/path/to/file
 
- deletes all versions of a file
+deletes all versions of a file
 
 ## List Files in path LIKE /path/to%:
 
- curl https://baxx.dev/ls/$TOKEN/path/to
+curl https://baxx.dev/ls/$TOKEN/path/to
 
- use -H "Accept: application/json" if you want json back by default it
- prints human readable text
+use -H "Accept: application/json" if you want json back by default it
+prints human readable text
 
 
 WriteOnly tokens require BasicAuth and /protected prefix.
 
 ## Download from WriteOnly token:
 
- curl -u {{ .Email }} \
-   https://baxx.dev/protected/io/$TOKEN/path/to/file
+curl -u {{ .Email }} \
+ https://baxx.dev/protected/io/$TOKEN/path/to/file
 
 ## Delete with WriteOnly token:
 
- curl -u {{ .Email }} -XDELETE \
-   https://baxx.dev/io/$TOKEN/path/to/file
+curl -u {{ .Email }} -XDELETE \
+ https://baxx.dev/io/$TOKEN/path/to/file
 
 ## List with WriteOnly token:
 
- curl -u {{ .Email }} \
-   https://baxx.dev/protected/ls/$TOKEN/path/to/
+curl -u {{ .Email }} \
+ https://baxx.dev/protected/ls/$TOKEN/path/to/
 
 
 # Profile Management
 
 ## Register:
 
- curl -d '{"email":"{{.Email}}", "password":"mickey mouse"}' \
-  https://baxx.dev/register | json_pp
+curl -d '{"email":"{{.Email}}", "password":"mickey mouse"}' \
+ https://baxx.dev/register | json_pp
 
 ## Change Password
 
- curl -u {{.Email}} -d'{"new_password": "donald mouse"}' \
-  https://baxx.dev/protected/replace/password | json_pp
+curl -u {{.Email}} -d'{"new_password": "donald mouse"}' \
+ https://baxx.dev/protected/replace/password | json_pp
 
- (use https://www.xkcd.com/936/)
+(use https://www.xkcd.com/936/)
 
 ## Change Email
 
- curl -u {{.Email}} -d'{"new_email": "x@example.com"}' \
-  https://baxx.dev/protected/replace/email | json_pp
+curl -u {{.Email}} -d'{"new_email": "x@example.com"}' \
+ https://baxx.dev/protected/replace/email | json_pp
 
- It will also send new verification email, you can also use the
- replace/email endpoint to resend the verification email.
+It will also send new verification email, you can also use the
+replace/email endpoint to resend the verification email.
 
 ## User Status
 
- curl -u {{.Email}} -XPOST https://baxx.dev/protected/status
+curl -u {{.Email}} -XPOST https://baxx.dev/protected/status
 
- shows things like
-  * is the email verified
-  * is subscription active [ not done yet ]
-  * current tokens
-  * size used
+shows things like
+ * is the email verified
+ * is subscription active [ not done yet ]
+ * current tokens
+ * size used
 
 # Encryption
 
- Your data is compressed and encrypted when received, the encryption
- key is auto generated uuid, and the purpose of the encryption is
- simply to obscure the data in case the machines are hacked, hacker
- will have to also get access to the database as well.
+Your data is compressed and encrypted when received, the encryption
+key is auto generated uuid, and the purpose of the encryption is
+simply to obscure the data in case the machines are hacked, hacker
+will have to also get access to the database as well.
 
- Anyway, dont trust it and use encryption when uploading.
+Anyway, dont trust it and use encryption when uploading.
 
 --
 baxx.dev
@@ -192,11 +193,11 @@ var EMAIL_VALIDATION = Parse(`Hi,
 
 this is the verification link:
 
-  https://baxx.dev/verify/{{.ID}}
+https://baxx.dev/verify/{{.ID}}
 
 You can check the account status with:
 
-  curl -u {{.Email}} -XPOST https://baxx.dev/protected/status | json_pp
+curl -u {{.Email}} -XPOST https://baxx.dev/protected/status | json_pp
 
 PS:
 It is very likely that this email goes to the spam folder because it
@@ -216,7 +217,7 @@ If you want to cancel you have to do that in your paypal account.
 
 You can check the account status with:
 
-  curl -u {{.Email}} -XPOST https://baxx.dev/protected/status | json_pp
+curl -u {{.Email}} -XPOST https://baxx.dev/protected/status | json_pp
 
 --
 baxx.dev
--- help/pitch.go
+++ help/pitch.go
@@ -2,7 +2,7 @@ package help
 
 var PITCH = Parse(`# baxx.dev is a simple backup service with unix philosophy in mind
 
- tackling what is fundamental problem with backups:
+Tackling the fundamental problems with backups:
   * anomaly detection
   * notifications
   * alerts
@@ -15,22 +15,22 @@ needed the data it was either corrupt or 0 bytes, or was missing.
 
 ## Alpha:
 
- baxx.dev is still alpha, use at your own risk
- no guarnatees about the data [yet]
+baxx.dev is still alpha, use at your own risk
+no guarnatees about the data [yet]
 
 ## Why charging during alpha:
 
- Because I want to see if someone really cares about this.
- Lets work together to make usable backup service!
+Because I want to see if someone really cares about this.
+Lets work together to make usable backup service!
 
 ## Encryption
 
- Your data is compressed and encrypted when received, the encryption
- key is auto generated uuid, and the purpose of the encryption is
- simply to obscure the data in case the machines are hacked, hacker
- will have to also get access to the database as well.
+Your data is compressed and encrypted when received, the encryption
+key is auto generated uuid, and the purpose of the encryption is
+simply to obscure the data in case the machines are hacked, hacker
+will have to also get access to the database as well.
 
- Anyway, dont trust it and use encryption when sending data.
+Anyway, dont trust it and use encryption when sending data.
 
 ## End goal
 
@@ -43,4 +43,9 @@ needed the data it was either corrupt or 0 bytes, or was missing.
 
 * easy configuration
   easy and intuitive rules that can be shared
+
+## Open Source
+
+The whole thing will be open sourced (soon)
+
 `)
