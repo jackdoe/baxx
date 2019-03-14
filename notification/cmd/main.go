5b977f575bfe531d1311eea01ee49c6a6b8b9ed8
borislav nikolov <jack@sofialondonmoskva.com>
2019-03-14 01:45:10 +0100

    try to send

--- notification/cmd/main.go
+++ notification/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/jackdoe/baxx/common"
 	"github.com/jackdoe/baxx/file"
@@ -35,9 +36,34 @@ func sendNotificationEmails(db *gorm.DB) {
 	if sendgrid == "" {
 		log.Fatalf("empty BAXX_SENDGRID_KEY")
 	}
+	toSend := []*common.EmailQueueItem{}
+	if err := db.Where("sent = ?", false).Order("asc").Find(&toSend).Error; err != nil {
+		log.Fatal(err)
+	}
+	for _, m := range toSend {
+		u := &user.User{}
+		if err := db.Where("user_id = ?", m.UserID).First(&u).Error; err != nil {
+			log.Fatal(err)
+		}
+		if u.EmailVerified == nil {
+			log.Infof("skipping notification for %s, unverified email", u.Email)
+			continue
+		}
 
-	for {
+		err := common.Sendmail(sendgrid, common.Message{
+			From:    "jack@baxx.dev",
+			To:      []string{u.Email},
+			Subject: m.EmailSubject,
+			Body:    m.EmailText,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		m.SentAt = time.Now()
+		if err := db.Save(m).Error; err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
