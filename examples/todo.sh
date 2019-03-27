7c331668cbaebf0744a2b06e086cb698aec6fb78
borislav nikolov <jack@sofialondonmoskva.com>
2019-03-27 21:25:34 +0100

    improve the todo example

--- examples/todo.sh
+++ examples/todo.sh
@@ -1,39 +1,51 @@
-todo_set() {
+_todo_set() {
     curl -f -s --data-binary @- https://baxx.dev/io/$BAXX_TOKEN/todo.txt > /dev/null
 }
 
-todo_get() {
-    curl https://baxx.dev/io/$BAXX_TOKEN/todo.txt -f -s || (echo -n | todo_set > /dev/null)
+todo_list() {
+    curl https://baxx.dev/io/$BAXX_TOKEN/todo.txt -f -s || (echo -n | _todo_set > /dev/null)
 }
 
 todo_add() {
-    { todo_get && echo [ $(date) ] $* } | todo_set && todo_get
+    if [ $# -eq 0 ]; then
+        echo "usage: $0 item"
+    else
+        { todo_list && echo [ $(date) ] $* } | _todo_set && todo_list
+    fi
 }
 
 todo_done() {
-    (
-        ITEM=$*
-        todo_get | \
-            while read -r line
-            do
-                case "$line" in
-                    *$ITEM) echo $line [ $(date) ] ;;
-                    *) echo $line ;;
-                esac
-            done
-    ) | todo_set && todo_get
+    if [ $# -eq 0 ]; then
+        echo "usage: $0 item"
+    else
+        (
+            ITEM=$*
+            todo_list | \
+                while read -r line
+                do
+                    case "$line" in
+                        *$ITEM) echo $line [ $(date) ] ;;
+                        *) echo $line ;;
+                    esac
+                done
+        ) | _todo_set && todo_list
+    fi
 }
 
 todo_delete_matching() {
-    (
-        ITEM=$*
-        todo_get | \
-            while read -r line
-            do
-                case "$line" in
-                    *$ITEM*) ;;
-                    *) echo $line ;;
-                esac
-            done
-    ) | todo_set && todo_get
+    if [ $# -eq 0 ]; then
+        echo "usage: $0 item"
+    else
+        (
+            ITEM=$*
+            todo_list | \
+                while read -r line
+                do
+                    case "$line" in
+                        *$ITEM*) ;;
+                        *) echo $line ;;
+                    esac
+                done
+        ) | _todo_set && todo_list
+    fi
 }
