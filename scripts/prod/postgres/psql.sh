a55404f1f12ca63df7f41fd85d5a72023840ffef
borislav nikolov <jack@sofialondonmoskva.com>
2019-04-01 09:24:46 +0200

    use the env pasword

--- scripts/prod/postgres/psql.sh
+++ scripts/prod/postgres/psql.sh
@@ -1,1 +1,2 @@
-docker run --rm -it --net=host postgres:11-alpine psql -h 127.0.0.1 -U root -W baxx
+source /root/.pass
+docker run -e PGPASSWORD="$POSTGRES_PASSWORD" --rm -it --net=host postgres:11-alpine psql -h 127.0.0.1 -U root baxx
