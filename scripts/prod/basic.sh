3634f6d8c5b426f2d4c075b7b390466977df6e5e
borislav nikolov <jack@sofialondonmoskva.com>
2019-04-01 08:01:24 +0200

    forgot to push the status script changes

--- scripts/prod/basic.sh
+++ scripts/prod/basic.sh
@@ -3,10 +3,13 @@
 apt-get update
 apt-get upgrade
 apt-get install apt-transport-https ca-certificates curl gnupg-agent ntp git software-properties-common ufw
+snap install go --classic
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository  "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs)  stable"
-apt-get update
-apt-get install docker-ce
+apt-get update -y
+apt-get install -y docker-ce
+
+
 
 
 ufw default deny outgoing comment 'deny all outgoing traffic'
--- scripts/prod/baxx/start-status.sh
+++ scripts/prod/baxx/start-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+source /root/.pass
 docker stop baxx-status
-docker stop
+docker rm baxx-status
 docker run \
        --net=host \
        --privileged \
@@ -9,4 +9,4 @@ docker run \
        -e BAXX_SLACK_PANIC="$BAXX_SLACK_PANIC" \
        -e BAXX_SLACK_MONITORING="$BAXX_SLACK_MONITORING" \
        -e BAXX_POSTGRES="$BAXX_POSTGRES" \
-       jackdoe/baxx:1.2 /baxx/notification_run -debug
+       jackdoe/baxx:1.2 /baxx/status -debug
