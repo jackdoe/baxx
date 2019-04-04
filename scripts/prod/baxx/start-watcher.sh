a63be18a8dd1263a0d7d9524d530018011bf0200
borislav nikolov <jack@sofialondonmoskva.com>
2019-04-04 15:15:52 +0200

    no debug

--- scripts/prod/baxx/start-watcher.sh
+++ scripts/prod/baxx/start-watcher.sh
@@ -11,4 +11,4 @@ docker run \
        -e BAXX_SLACK_PANIC="$BAXX_SLACK_PANIC" \
        -e BAXX_SLACK_MONITORING="$BAXX_SLACK_MONITORING" \
        -e BAXX_POSTGRES="$BAXX_POSTGRES" \
-       jackdoe/baxx:1.9.2 /baxx/who_watches_the_watchers -debug
+       jackdoe/baxx:1.9.2 /baxx/who_watches_the_watchers 
