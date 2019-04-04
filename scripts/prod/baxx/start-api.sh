69671fb86e03848e63e41ff6ab36cc39e21ba6c1
borislav nikolov <jack@sofialondonmoskva.com>
2019-04-04 17:18:06 +0200

    bump

--- scripts/prod/baxx/start-api.sh
+++ scripts/prod/baxx/start-api.sh
@@ -11,4 +11,4 @@ docker run \
        -e BAXX_SLACK_PANIC="$BAXX_SLACK_PANIC" \
        -e BAXX_POSTGRES="$BAXX_POSTGRES" \
        -e BAXX_JUDOC_URL="http://localhost:9122/" \
-       jackdoe/baxx:1.9.2 /baxx/api -debug
+       jackdoe/baxx:1.9.3 /baxx/api -debug
