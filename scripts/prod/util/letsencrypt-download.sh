d345fa3b8cc05ea8f83539d1cccec799d3574b6a
borislav nikolov <jack@sofialondonmoskva.com>
2019-04-06 14:18:05 +0200

    use verbose tar

--- scripts/prod/util/letsencrypt-download.sh
+++ scripts/prod/util/letsencrypt-download.sh
@@ -4,6 +4,6 @@ source /root/.bashrc
 
 curl https://baxx.dev/io/$BAXX_TOKEN/letsencrypt.tar | \
     encrypt -k /root/.pw -d | \
-    tar -xf - -C /
+    tar -xvf - -C /
 
 echo ok | curl -s -d@- "https://baxx.dev/io/$BAXX_TOKEN/letsencrypt-downloaded-bb.monitor?age=3600"
--- scripts/prod/util/letsencrypt-upload.sh
+++ scripts/prod/util/letsencrypt-upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /root/.bashrc
 
-tar -cf - /etc/letsencrypt | \
+tar -cvf - /etc/letsencrypt | \
     encrypt -k /root/.pw | \
     curl --data-binary @- "https://baxx.dev/io/$BAXX_TOKEN/letsencrypt.tar?age=3600&delta=80"
